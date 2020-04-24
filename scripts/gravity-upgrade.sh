c6e30a67a91a2d8e2f587ac9a8a5891aaa4c7da1
dmitri <deemok@gmail.com>
2020-04-24 13:24:05 +0200

    Use correct path to resources YAML during upgrade

--- scripts/gravity-upgrade.sh
+++ scripts/gravity-upgrade.sh
@@ -7,6 +7,6 @@ echo "Changeset: $RIG_CHANGESET"
 
 if rig status $RIG_CHANGESET --retry-attempts=1 --retry-period=1s --quiet; then exit 0; fi
 
-rig upsert -f /var/lib/gravity/resources/dns.yaml
+rig upsert -f /gravity/wormhole.yaml
 rig status $RIG_CHANGESET --retry-attempts=120 --retry-period=1s --debug
 rig freeze
